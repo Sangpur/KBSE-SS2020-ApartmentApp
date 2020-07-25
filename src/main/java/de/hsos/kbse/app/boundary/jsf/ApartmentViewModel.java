68e09c2f60058f3e6a6b1f0734566bf49c2cbb8d
Annika <a.limbrock@web.de>
2020-07-25 15:23:50 +0200

    Mitgliederliste angepasst

--- src/main/java/de/hsos/kbse/app/boundary/jsf/ApartmentViewModel.java
+++ src/main/java/de/hsos/kbse/app/boundary/jsf/ApartmentViewModel.java
@@ -14,6 +14,7 @@ import de.hsos.kbse.app.enums.MemberColor;
 import de.hsos.kbse.app.enums.MemberRole;
 import de.hsos.kbse.app.enums.ValidationGroup;
 import de.hsos.kbse.app.util.AppException;
+import de.hsos.kbse.app.util.Condition;
 import de.hsos.kbse.app.util.General;
 import de.hsos.kbse.app.util.Logable;
 import java.io.Serializable;
@@ -32,9 +33,12 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import javax.servlet.http.HttpSession;
 import javax.validation.ConstraintViolation;
+import javax.validation.Valid;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 /**
  *
@@ -58,11 +62,15 @@ public class ApartmentViewModel implements Serializable {
     
     private Apartment apartment;
     private List<Member> members;
+    @Valid
     private Member currentMember = null;
+    @NotNull(groups = {General.class, Condition.class}, message="Das Passwort darf nicht leer sein!")
+    @Size(groups = {General.class, Condition.class}, min=3, max=50, message="Das Passwort muss zwischen 3 und 50 Zeichen liegen!")
     private String newpassword = "";
+    @NotNull(groups = {General.class, Condition.class}, message="Die Passwort-Wiederholung darf nicht leer sein!")
     private String repassword = "";
     private Boolean editMode = false;
-    private MemberColor[] colors = {MemberColor.RED, MemberColor.GREEN, MemberColor.BLUE, MemberColor.YELLOW, MemberColor.PINK, MemberColor.ORANGE};
+    private MemberColor[] colors;
     private MemberRole[] roles = {MemberRole.ADMIN, MemberRole.USER};
     
     /* -------------------------------------- METHODEN PUBLIC ------------------------------------- */
@@ -73,6 +81,7 @@ public class ApartmentViewModel implements Serializable {
         this.repository = repository;
         this.memberRepository = memberRepository;
         this.conversation = conversation;
+        this.colors = MemberColor.values();
         this.initApartmentByID(getLoggedInMember().getApartmentID()); // TODO: Bei Login-Prozess entsprechende ID setzen
     }
     
@@ -83,88 +92,102 @@ public class ApartmentViewModel implements Serializable {
         validator = factory.getValidator();
     }
     
-    public String goToAddMember() {
+    public String addMember() {
         this.currentMember = new Member();
         this.repassword = "";
         this.editMode = false;
         this.currentMember.setApartmentID(apartment.getId());
         this.currentMember.setMemberRole(MemberRole.USER);
-        this.currentMember.getDetails().setColor(MemberColor.GREEN);
+        /* Berechnung der naechsten Mitglieds-Farbe anhand der Anzahl der Mitglieder */
+        int amountMembers = this.members.size();
+        int colorIndex = amountMembers % MemberColor.values().length;
+        MemberColor color = this.colors[colorIndex];
+        this.currentMember.getDetails().setColor(color);
         return "members-add";
     }
     
-    public String editMember(Long id) {
-        this.currentMember = members.stream().filter(x -> x.getId().equals(id)).findFirst().orElse(null);
+    public String editMember(Member member) {
+        /*this.currentMember = members.stream().filter(x -> x.getId().equals(id)).findFirst().orElse(null);
         if(currentMember == null){
             FacesContext.getCurrentInstance().addMessage("Error", new FacesMessage("Member with id " + id + "could not be found."));
             return "";
-        }
+        }*/
+        this.currentMember = member;
         this.newpassword = "";
         this.repassword = "";
         this.editMode = true;
         return "members-add";
     }
     
-    public String deleteMember(Long id) {
-        this.currentMember = members.stream().filter(x -> x.getId().equals(id)).findFirst().orElse(null);
-        if(currentMember == null){
-            FacesContext.getCurrentInstance().addMessage("Error", new FacesMessage("Member with id " + id + "could not be found."));
-            return "";
-        }
+    public String changePassword(Member member) {
+        System.out.println("Change Password");
+        return "";
+    }
+    
+    public String deleteMember(Member member) {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        this.currentMember = member;
         try {
+            /* Bestehendes Mitglied wird auch beim Loeschen in der Datenbank geupdaten und der deleted-Status
+             * auf true gesetzt. So wird verhindert, dass ein kaskadierendes Loeschen von Eintraegen notwendig
+             * ist und von dem Mitglied erstellte Beitraege erhalten bleiben. */
             currentMember.setDeleted(true);
             this.memberRepository.updateMember(currentMember);
             initApartmentByID(apartment.getId());
-            FacesContext.getCurrentInstance().addMessage("Succes", new FacesMessage("Member was deleted."));
-            FacesContext facesContext = FacesContext.getCurrentInstance();
-            facesContext.getExternalContext().redirect("/ApartmentApp/faces/pages/members.xhtml");
-        } catch (Exception e) {
-            Logger.getLogger(ApartmentViewModel.class.getName()).log(Level.SEVERE, null, e);
-            FacesContext.getCurrentInstance().addMessage("Error", new FacesMessage("Member with id " + id + "could not be deleted."));
+            String message = "Mitglied erfolgreich geloescht!";
+            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Information.", message);
+            return "members";
+        } catch (AppException ex) {
+            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error.", ex.getMessage());
+            facesContext.addMessage("Error",msg);
         }
-        initApartmentByID(apartment.getId());
         return "";
     }
     
     public String saveMember() {
-        if(!editMode){ // Add new Member to Apartment
-            if(currentMember.getPassword() != null && currentMember.getPassword().equals(repassword)){
-                if(this.validateInput(ValidationGroup.GENERAL)) { // Gueltige Eingabe
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        if(this.validateInput(ValidationGroup.GENERAL)) { // Gueltige Eingabe
+            if(!editMode){  // Member hinzufuegen
+                if(currentMember.getPassword().equals(this.repassword)){
                     try {
+                        /* Anlegen eines neuen Members in der Datenbank */
                         currentMember.getDetails().setCashBalance(new BigDecimal(0));
                         this.memberRepository.createMember(currentMember);
-                        initApartmentByID(apartment.getId());
-                        FacesContext.getCurrentInstance().addMessage("Succes", new FacesMessage("Member was created."));
+                        /* FacesMessage fuer erfolgreiches Anlegen eines neuen Mitglieds */
+                        String message = "Mitglied erfolgreich angelegt!";
+                        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Information.", message);
+                        facesContext.addMessage("Information",msg);
                         return resetMember();
                     } catch (AppException ex) {
-                        Logger.getLogger(ApartmentViewModel.class.getName()).log(Level.SEVERE, null, ex);
-                        FacesContext.getCurrentInstance().addMessage("Error", new FacesMessage("Member could not be added to apartment."));
+                        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error.", ex.getMessage());
+                        facesContext.addMessage("Error",msg);
                     }
-                }
-            } else {
-                FacesContext.getCurrentInstance().addMessage("Error", new FacesMessage("Passwords are not identical."));
-                return "";
-            }
-        } else { // Save existing Member
-            if(newpassword != null && newpassword.length() > 0 ){
-                if(newpassword.equals(repassword)){
-                    currentMember.setPassword(newpassword);
                 } else {
-                FacesContext.getCurrentInstance().addMessage("Error", new FacesMessage("Passwords are not identical."));
-                return "";
+                    String message = "Die Passwörter müssen identisch sein!";
+                    FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_WARN, "Constraint.", message);
+                    facesContext.addMessage("Constraint",msg);
+                }
+            } else {        // Bestehenden Member updaten
+                if(newpassword.equals(this.repassword)){
+                    currentMember.setPassword(newpassword);
+                    try {
+                        /* Bestehendes Mitglied in der Datenbank updaten */
+                        this.memberRepository.updateMember(currentMember);
+                        /* FacesMessage fuer erfolgreiches Updates eines Mitglieds */
+                        String message = "Mitglied erfolgreich aktualisiert!";
+                        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Information.", message);
+                        facesContext.addMessage("Information",msg);
+                        return resetMember();
+                    } catch (AppException ex) {
+                        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error.", ex.getMessage());
+                        facesContext.addMessage("Error",msg);
+                    }
+                } else {
+                    String message = "Die Passwörter müssen identisch sein!";
+                    FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_WARN, "Constraint.", message);
+                    facesContext.addMessage("Constraint",msg);
                 }
             }
-            if(this.validateInput(ValidationGroup.GENERAL)) { // Gueltige Eingabe
-                try {
-                    this.memberRepository.updateMember(currentMember);
-                    initApartmentByID(apartment.getId());
-                    FacesContext.getCurrentInstance().addMessage("Succes", new FacesMessage("Member was updated."));
-                    return resetMember();
-                } catch (AppException ex) {
-                    Logger.getLogger(ApartmentViewModel.class.getName()).log(Level.SEVERE, null, ex);
-                    FacesContext.getCurrentInstance().addMessage("Error", new FacesMessage("Member informations could not be updated."));
-                }
-            } 
         }
         return "";
     }
@@ -192,6 +215,10 @@ public class ApartmentViewModel implements Serializable {
         return getLoggedInMember().getMemberRole().equals(MemberRole.ADMIN);
     }
     
+    public Boolean isLoggedInMember(Member member) {
+        return this.getLoggedInMember().getId().equals(member.getId());
+    }
+    
     /* ------------------------------------- METHODEN PRIVATE ------------------------------------- */
     
     private void initApartmentByID(Long id) {
@@ -199,8 +226,9 @@ public class ApartmentViewModel implements Serializable {
             this.apartment = this.repository.findApartment(id);
             this.members = this.memberRepository.getAllMembersFrom(id);
         } catch(AppException ex) {
-            String msg = ex.getMessage();
-            FacesContext.getCurrentInstance().addMessage("Error", new FacesMessage(msg));
+            FacesContext facesContext = FacesContext.getCurrentInstance();
+            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error.", ex.getMessage());
+            facesContext.addMessage("Error",msg);
         }
     }
     
@@ -210,35 +238,17 @@ public class ApartmentViewModel implements Serializable {
          * sind. Dieses Set ist leer, falls die Eingabe gueltig ist. Durch die Gruppierung der Constraints, hier General.class, besteht die
          * Moeglichkeit, die Validation erst dann auszufuehren, wenn die entsprechende Gruppe direkt durch das Programm angesprochen wird.
          * s. https://www.baeldung.com/javax-validation-groups */
-        Set<ConstraintViolation<Member>> constraintViolations = null;
+        Set<ConstraintViolation<ApartmentViewModel>> constraintViolations = null;
         if(group == ValidationGroup.GENERAL) {
-            constraintViolations = validator.validate(currentMember, General.class );
+            constraintViolations = validator.validate(this, General.class );
         }
-
         if(constraintViolations != null && constraintViolations.isEmpty()) {
-            /* Gueltige Eingabe for member constraints so now checking for member.details constaints */
-            Set<ConstraintViolation<MemberDetail>> detailConstraintViolations = null;
-            if(group == ValidationGroup.GENERAL) {
-                detailConstraintViolations = validator.validate(currentMember.getDetails(), General.class );
-            }
-
-            if(detailConstraintViolations != null && detailConstraintViolations.isEmpty()) {
-                /* Gueltige Eingabe */
-                return true;
-            } else {
-                /* Ungueltige Eingabe */
-                FacesContext facesContext = FacesContext.getCurrentInstance();
-                Iterator<ConstraintViolation<MemberDetail>> iter = detailConstraintViolations.iterator();
-                while (iter.hasNext()) {
-                    FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_WARN, "Constraint.", iter.next().getMessage());
-                    facesContext.addMessage("Constraint Violation",msg);
-                }
-                return false;
-            }
+            /* Gueltige Eingabe */
+            return true;
         } else {
             /* Ungueltige Eingabe */
             FacesContext facesContext = FacesContext.getCurrentInstance();
-            Iterator<ConstraintViolation<Member>> iter = constraintViolations.iterator();
+            Iterator<ConstraintViolation<ApartmentViewModel>> iter = constraintViolations.iterator();
             while (iter.hasNext()) {
                 FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_WARN, "Constraint.", iter.next().getMessage());
                 facesContext.addMessage("Constraint Violation",msg);
